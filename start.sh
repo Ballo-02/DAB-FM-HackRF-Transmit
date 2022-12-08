39abb1578f634c1413b42fd57d1f8bcf3fa96217
OwenB <owen.ball@skyships.co.uk>
2022-12-08 16:04:43 +0000

    Updated for V3

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
-cd /home/anyone/Documents/radio/DAB-FM-HackRF-Transmit &
-sudo python3 /home/anyone/Documents/radio/DAB-FM-HackRF-Transmit/main.py &
-sudo python3 /home/anyone/Documents/radio/DAB-FM-HackRF-Transmit/fail_safe.py &
+cd /home/anyone/Documents/radio/DAB-FM-HackRF-Transmit && sudo python3 /home/anyone/Documents/radio_simulation/DAB-FM-HackRF-Transmit/main.py &
+cd /home/anyone/Documents/radio/DAB-FM-HackRF-Transmit && sudo python3 /home/anyone/Documents/radio_simulation/DAB-FM-HackRF-Transmit/fail_safe.py &
